334aaf515d77e676ec5f4264840b94dfe3d64582
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-09 22:56:22 +0600

    update admin service

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,12 +2,20 @@ import { Request, Response } from "express";
 import { adminService } from "./admin.service";
 
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await adminService.getAllFromDB();
-  res.status(200).json({
-    success: true,
-    message: "Admin fetched successfully!",
-    data: result,
-  });
+  try {
+    const result = await adminService.getAllFromDB(req.query);
+    res.status(200).json({
+      success: true,
+      message: "Admin fetched successfully!",
+      data: result,
+    });
+  } catch (error:any) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error,
+    });
+  }
 };
 export const adminController = {
   getAllFromDB,
--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,10 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-const getAllFromDB= async () => {
-    const result = await prisma.admin.findMany();
-    return result
-}
+const getAllFromDB = async (query: any) => {
+  const andCondition: Prisma.AdminWhereInput[] = [];
+  const adminSearchableFields = ["name", "email"];
+  if (query?.searchTerm) {
+    andCondition.push({
+      OR: adminSearchableFields.map((field) => ({
+        [field]: { contains: query.searchTerm, mode: "insensitive" },
+      })),
+    });
+  }
+  const whereCondition: Prisma.AdminWhereInput = {
+    AND: andCondition,
+  };
+  const result = await prisma.admin.findMany({
+    where: whereCondition,
+  });
+  return result;
+};
 
 export const adminService = {
-    getAllFromDB
-}
+  getAllFromDB,
+};
