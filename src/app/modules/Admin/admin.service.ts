d664ad37fb6ddfc1e4380d54542a4626055f1ee1
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-10 20:33:52 +0600

    added advance filter code structure

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,12 +1,23 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 const getAllFromDB = async (query: any) => {
+  const { searchTerm, ...filterData } = query;
   const andCondition: Prisma.AdminWhereInput[] = [];
   const adminSearchableFields = ["name", "email"];
   if (query?.searchTerm) {
     andCondition.push({
       OR: adminSearchableFields.map((field) => ({
         [field]: { contains: query.searchTerm, mode: "insensitive" },
+      })),
+    });
+  }
+  if (Object.keys(filterData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: filterData[key],
+          // mode: "insensitive",
+        },
       })),
     });
   }
