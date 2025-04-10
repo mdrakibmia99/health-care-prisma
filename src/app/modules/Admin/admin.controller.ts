0d1675bebb8d36d751bf486fc15759fbe83ae9d6
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-10 22:48:42 +0600

    updated admin controller

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,18 +1,20 @@
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
-const pick=(obj,keys)=>{
+import pick from "../../shared/pick";
+import { adminFilterableFields } from "./admin.constant";
 
-}
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
-    pick(req.query, ["searchTerm", "name", "email"]);
-    const result = await adminService.getAllFromDB(req.query);
+   
+    const filter= pick(req.query,adminFilterableFields );
+    const options= pick(req.query,["limit","page",'sortBy','sortOrder'] );
+    const result = await adminService.getAllFromDB(filter,options);
     res.status(200).json({
       success: true,
       message: "Admin fetched successfully!",
       data: result,
     });
-  } catch (error:any) {
+  } catch (error: any) {
     res.status(500).json({
       success: false,
       message: error?.name || "Something went wrong",
