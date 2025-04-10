2495a0b1cef39c3147acd9957daf65fcf755645e
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-10 20:34:05 +0600

    specific data get only

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 import { adminService } from "./admin.service";
+const pick=(obj,keys)=>{
 
+}
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
+    pick(req.query, ["searchTerm", "name", "email"]);
     const result = await adminService.getAllFromDB(req.query);
     res.status(200).json({
       success: true,
