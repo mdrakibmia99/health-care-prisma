b2d43e6daafb0980dcb5ac580882f8698afe9244
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-12 02:40:15 +0600

    create new controller for admin single data get

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -5,10 +5,34 @@ import { adminFilterableFields } from "./admin.constant";
 
 const getAllFromDB = async (req: Request, res: Response) => {
   try {
-   
-    const filter= pick(req.query,adminFilterableFields );
-    const options= pick(req.query,["limit","page",'sortBy','sortOrder'] );
-    const result = await adminService.getAllFromDB(filter,options);
+    const filter = pick(req.query, adminFilterableFields);
+    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+    const result = await adminService.getAllFromDB(filter, options);
+    res.status(200).json({
+      success: true,
+      message: "Admin fetched successfully!",
+      data: result.data,
+      meta: result.meta,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error?.name || "Something went wrong",
+      error,
+    });
+  }
+};
+
+const getByIdFromDB = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const result = await adminService.getByIdFromDB(id);
+    if (!result) {
+      return res.status(404).json({
+        success: false,
+        message: "Admin not found!",
+      });
+    }
     res.status(200).json({
       success: true,
       message: "Admin fetched successfully!",
@@ -24,4 +48,5 @@ const getAllFromDB = async (req: Request, res: Response) => {
 };
 export const adminController = {
   getAllFromDB,
+  getByIdFromDB,
 };
