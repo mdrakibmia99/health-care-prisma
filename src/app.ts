b41733d6b063d03be0f83939614e1ec7bd32708f
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-12 18:52:33 +0600

    added global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from 'express';
-import cors from 'cors';
-import router from './app/routes';
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import router from "./app/routes";
+import httpStatus from "http-status";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 app.use(cors());
@@ -9,12 +11,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-    res.send({
-        Message: "Ph health care server.."
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Ph health care server..",
+  });
 });
 
-app.use('/api/v1', router);
+app.use("/api/v1", router);
+
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
 
 export default app;
