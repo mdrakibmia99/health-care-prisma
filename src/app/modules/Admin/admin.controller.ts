8192dc7337a931c0197ee82141b48588a1a64798
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-13 21:09:48 +0600

    catchAsync file separet

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -4,16 +4,9 @@ import pick from "../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../shared/sendResponse";
 import httpStatus from "http-status-codes";
+import catchAsync from "../../shared/catchAsync";
 
-const catchAsync = (fn: RequestHandler) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await fn(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
+
 const getAllFromDB: RequestHandler = catchAsync(async (req, res) => {
   const filter = pick(req.query, adminFilterableFields);
   const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
