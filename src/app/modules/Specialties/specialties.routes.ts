2ac5a509d510dc51a9a4514721663a59049926b5
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-29 13:31:54 +0600

    specialties delete done

--- src/app/modules/Specialties/specialties.routes.ts
+++ src/app/modules/Specialties/specialties.routes.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import { fileUploader } from "../../helpers/fileUploader";
 import { SpecialtiesValidation } from "./specialties.validations";
 import { SpecialtiesController } from "./specialties.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
 
 const router = express.Router();
 // Retrieve Specialties Data
@@ -23,6 +25,20 @@ router.post(
     req.body = SpecialtiesValidation.create.parse(JSON.parse(req.body.data));
     return SpecialtiesController.insertIntoDB(req, res, next);
   }
+);
+// Delete Specialties Data by ID
+
+/**
+- Develop an API endpoint to delete specialties by ID.
+- Implement an HTTP DELETE endpoint accepting the specialty ID.
+- Delete the specialty from the database and return a success message.
+- ENDPOINT: /specialties/:id
+*/
+
+router.delete(
+  '/:id',
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  SpecialtiesController.deleteFromDB
 );
 
 export const SpecialtiesRoutes = router;
