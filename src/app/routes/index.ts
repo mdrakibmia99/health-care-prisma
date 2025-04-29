f261201de83456063eacf56d56d3aea0f6e4c28c
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-04-29 15:30:32 +0600

    for patient like doctor get delete update done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,7 @@ import { adminRoutes } from "../modules/Admin/admin.routes";
 import { AuthRoutes } from "../modules/Auth/auth.routers";
 import { SpecialtiesRoutes } from "../modules/Specialties/specialties.routes";
 import { DoctorRoutes } from "../modules/Doctor/doctor.routes";
+import { PatientRoutes } from "../modules/Patient/patient.route";
 
 const router= Router()
 const moduleRoutes=[
@@ -26,6 +27,10 @@ const moduleRoutes=[
     {
         path: '/doctor',
         route: DoctorRoutes
+    },
+    {
+        path: '/patient',
+        route: PatientRoutes
     },
    
 
