0e27d1d631aff890f216c79247144a081a0a1f88
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-05-03 03:42:31 +0600

    get by id route create in schedule

--- src/app/modules/Schedule/schedule.routes.ts
+++ src/app/modules/Schedule/schedule.routes.ts
@@ -1,22 +1,26 @@
-import express from 'express';;
+import express from 'express';
+import { ScheduleController } from './schedule.controller';
 import auth from '../../middlewares/auth';
 import { UserRole } from '@prisma/client';
 
-
 const router = express.Router();
-
-
 
 router.get(
     '/',
-    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR, UserRole.PATIENT),
-    DoctorScheduleController.getAllFromDB
+    auth(UserRole.DOCTOR),
+    ScheduleController.getAllFromDB
+);
+
+/**
+ * API ENDPOINT: /schedule/:id
+ * 
+ * Get schedule data by id
+ */
+router.get(
+    '/:id',
+    auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+    ScheduleController.getByIdFromDB
 );
 
 
-
-
-
-
-
-export const DoctorScheduleRoutes = router;
+export const ScheduleRoutes = router;
