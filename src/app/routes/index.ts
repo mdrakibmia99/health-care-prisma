4dd255e733a530596f5057ecf85f1e4fc4af9438
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-05-15 21:24:41 +0600

    fixed(route): route error fixed

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,37 +5,50 @@ import { AuthRoutes } from "../modules/Auth/auth.routers";
 import { SpecialtiesRoutes } from "../modules/Specialties/specialties.routes";
 import { DoctorRoutes } from "../modules/Doctor/doctor.routes";
 import { PatientRoutes } from "../modules/Patient/patient.route";
+import { ScheduleRoutes } from "../modules/Schedule/schedule.routes";
+import { DoctorScheduleRoutes } from "../modules/DoctorSchedule/doctorSchedule.route";
+import { AppointmentRoutes } from "../modules/Appointment/appointement.routes";
 
-const router= Router()
-const moduleRoutes=[
-    {
-        path:"/user",
-        route:userRoutes
-    },
-    {
-        path:"/admin",
-        route:adminRoutes
-    },
-    {
-        path: '/auth',
-        route: AuthRoutes
-    },
-    {
-        path: '/specialties',
-        route: SpecialtiesRoutes
-    },
-    {
-        path: '/doctor',
-        route: DoctorRoutes
-    },
-    {
-        path: '/patient',
-        route: PatientRoutes
-    },
-   
+const router = Router();
+const moduleRoutes = [
+  {
+    path: "/user",
+    route: userRoutes,
+  },
+  {
+    path: "/admin",
+    route: adminRoutes,
+  },
+  {
+    path: "/auth",
+    route: AuthRoutes,
+  },
+  {
+    path: "/specialties",
+    route: SpecialtiesRoutes,
+  },
+  {
+    path: "/doctor",
+    route: DoctorRoutes,
+  },
+  {
+    path: "/patient",
+    route: PatientRoutes,
+  },
+  {
+    path: "/schedule",
+    route: ScheduleRoutes,
+  },
+  {
+    path: "/doctor-schedule",
+    route: DoctorScheduleRoutes,
+  },
+  {
+    path: "/appointment",
+    route: AppointmentRoutes,
+  },
+];
 
-]
-
-moduleRoutes.forEach(route=>router.use(route.path,route.route))
+moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
