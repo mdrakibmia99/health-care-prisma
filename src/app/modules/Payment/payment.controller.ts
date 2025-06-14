7815544152bd5a02cdbf310795b327855df2b184
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-06-14 22:26:01 +0600

    Implement payment initialization controller

---
+++ src/app/modules/Payment/payment.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { StatusCodes } from "http-status-codes";
+
+const initPayment = catchAsync(async (req: Request, res: Response) => {
+    const { appointmentId } = req.params;
+    const result = await PaymentService.initPayment(appointmentId);
+    sendResponse(res, {
+        statusCode: StatusCodes.OK,
+        success: true,
+        message: 'Payment initiate successfully',
+        data: result,
+    });
+});
+
+
+
+export const PaymentController = {
+    initPayment,
+ 
+}
