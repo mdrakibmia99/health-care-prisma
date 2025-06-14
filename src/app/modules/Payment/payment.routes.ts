8a12c3f768efe424870d3dcb433e34633bd15ec0
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-06-14 22:25:47 +0600

    Add IPN validation route to Payment module

--- src/app/modules/Payment/payment.routes.ts
+++ src/app/modules/Payment/payment.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { PaymentController } from "./payment.controller";
 
 const router = Router();
 
+router.get(
+    '/ipn',
+    PaymentController.validatePayment
+)
 
 router.post(
     '/init-payment/:appointmentId',
