8eaf82e8db37e56ad1db86e1b378e04d687b2c35
mdrakibmia99 <rkrakibhasan680@gmail.com>
2025-06-16 01:43:36 +0600

    Refactor payment controller for consistent formatting and indentation

--- src/app/modules/Payment/payment.controller.ts
+++ src/app/modules/Payment/payment.controller.ts
@@ -5,27 +5,27 @@ import { StatusCodes } from "http-status-codes";
 import { PaymentService } from "./payment.service";
 
 const initPayment = catchAsync(async (req: Request, res: Response) => {
-    const { appointmentId } = req.params;
-    const result = await PaymentService.initPayment(appointmentId);
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: 'Payment initiate successfully',
-        data: result,
-    });
+  const { appointmentId } = req.params;
+  const result = await PaymentService.initPayment(appointmentId);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Payment initiate successfully",
+    data: result,
+  });
 });
 
 const validatePayment = catchAsync(async (req: Request, res: Response) => {
-    const result = await PaymentService.validatePayment(req.query);
-    sendResponse(res, {
-        statusCode: StatusCodes.OK,
-        success: true,
-        message: 'Payment validate successfully',
-        data: result,
-    });
+  const result = await PaymentService.validatePayment(req.query);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Payment validate successfully",
+    data: result,
+  });
 });
 
 export const PaymentController = {
-    initPayment,
-    validatePayment
-}
+  initPayment,
+  validatePayment,
+};
